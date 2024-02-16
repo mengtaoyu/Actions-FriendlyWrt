e40561c725c5f23b86e3d3153dfa69d30d6e8e69
mengtaoyu <45725396+mengtaoyu@users.noreply.github.com>
2024-02-16 10:56:17 +0800

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,3 +3,8 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+sed -i -e '$s/$/CONFIG_PACKAGE_kmod-netem=y/' configs/rockchip/01-nanopi
+sed -i -e '$s/$/CONFIG_PACKAGE_kmod-sched-core=y/' configs/rockchip/01-nanopi
+sed -i -e '$s/$/CONFIG_PACKAGE_kmod-sched=y/' configs/rockchip/01-nanopi
+tail -n 5 configs/rockchip/01-nanopi
